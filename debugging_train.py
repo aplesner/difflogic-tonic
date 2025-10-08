fixed the training of the mlp and cnn models. the error came from the autocast that was placed wrongly and thus also applied to the gradient computation

debugging_train.py
@@ -10,12 +10,15 @@ Usage:
     python3 minimal_test.py
 """
 
+import time
+from pathlib import Path
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
-from pathlib import Path
 
+from src import train as src_train, model as src_model, config as src_config, model_config as src_model_config
 
 class SimpleMLP(nn.Module):
     """Simple 2-layer MLP for testing"""
@@ -75,18 +78,27 @@ def train_epoch(model, dataloader, criterion, optimizer, device, epoch):
     correct = 0
     total = 0
 
+    config = src_config.Config()
+
     for batch_idx, (data, target) in enumerate(dataloader):
-        data, target = data.to(device), target.to(device)
 
-        optimizer.zero_grad()
-        output = model(data)
-        loss = criterion(output, target)
-        loss.backward()
-        optimizer.step()
+        loss, accuracy = src_train.train_step(model=model, batch=(data, target), criterion=criterion,
+                             optimizer=optimizer, config=config, batch_idx=batch_idx, device=device, dtype=torch.float32)
 
-        total_loss += loss.item()
-        pred = output.argmax(dim=1)
-        correct += (pred == target).sum().item()
+        # data, target = data.to(device), target.to(device)
+
+        # optimizer.zero_grad()
+        # output = model(data)
+        # loss = criterion(output, target)
+        # loss.backward()
+        # optimizer.step()
+
+        # total_loss += loss.item()
+        # pred = output.argmax(dim=1)
+        # correct += (pred == target).sum().item()
+        # total += target.size(0)
+        total_loss += loss
+        correct += int(accuracy * target.size(0))
         total += target.size(0)
 
         if batch_idx % 250 == 0:
@@ -98,6 +110,41 @@ def train_epoch(model, dataloader, criterion, optimizer, device, epoch):
     accuracy = 100. * correct / total
     avg_loss = total_loss / len(dataloader)
     return avg_loss, accuracy
+
+def train_epoch_2(
+        model: nn.Module, 
+        dataloader: DataLoader, 
+        criterion: nn.Module, 
+        optimizer: optim.Optimizer, 
+        batch_count: int, 
+    ) -> tuple[float, int, float]:
+    """Train for one epoch"""
+    model.train()
+    total_loss = 0.0
+    # device = model.parameters().__next__().device
+    dtype = torch.float16
+    config = src_config.Config()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    correct = 0
+    total = 0
+
+    print(f"  Training on device: {device}, dtype: {dtype}")
+
+    for batch_idx, batch in enumerate(dataloader):
+        loss, batch_correct, accuracy = src_train.train_step(
+            model=model, batch=batch, criterion=criterion, optimizer=optimizer, config=config, batch_idx=batch_idx, device=device, dtype=dtype
+        )
+        total_loss += loss
+        batch_count += 1
+        correct += batch_correct
+        total += batch[0].size(0)
+
+        if batch_idx % config.train.log_interval == 0:
+            print(f"  Batch {batch_idx}/{len(dataloader)}, Loss: {loss:.4f}, Accuracy: {100 * accuracy:.2f}%")
+
+    accuracy = 100. * correct / total if total > 0 else 0
+
+    return total_loss / len(dataloader), batch_count, accuracy
 
 
 def evaluate(model, dataloader, criterion, device):
@@ -159,17 +206,25 @@ def main():
     print()
 
     # Create model
-    model = SimpleMLP(input_size, hidden_size, num_classes).to(device)
+    config = src_config.Config()
+    mlp_config = src_model_config.MLPConfig(hidden_size=hidden_size, num_hidden_layers=1)
+    model = src_model.MLP(config=mlp_config, input_size=input_size, num_classes=num_classes).to(device)
+
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
     # Training loop
     print("Starting training...")
     print()
-
+    batch_count = 0
+    # config.train.dtype = torch.float32  # Use float32 for this test
     for epoch in range(1, num_epochs + 1):
-        train_loss, train_acc = train_epoch(
-            model, train_loader, criterion, optimizer, device, epoch
+        # train_loss, train_acc = train_epoch(
+        #     model, train_loader, criterion, optimizer, device, epoch
+        # )
+        train_loss, batch_count, train_acc = train_epoch_2(
+            model=model, dataloader=train_loader, criterion=criterion, 
+            optimizer=optimizer, batch_count=batch_count, 
         )
         test_loss, test_acc = evaluate(model, test_loader, criterion, device)
 

src/config.py
@@ -49,12 +49,12 @@ class TrainConfig(BaseModel):
 
     epochs: int = 3
     learning_rate: float = 1e-3
-    log_interval: int = 1
-    debugging_steps: int = 1000
+    log_interval: int = 250
+    debugging_steps: int = 10
     save_model: bool = False
-    checkpoint_interval_minutes: float = 5.0  # time-based checkpointing
+    checkpoint_interval_minutes: float = 10.0  # time-based checkpointing
     model_path: str = "models/"
-    device: str | torch.device = "cpu"
+    device: str | torch.device = "cuda"
     dtype: str | torch.dtype = "float16"  # options: float16, bfloat16, float32
     dataloader: DataLoaderConfig = DataLoaderConfig()
 

src/model.py
@@ -1,50 +1,61 @@
-from difflogic import LogicLayer, GroupSum
+try:
+    from difflogic import LogicLayer, GroupSum
+    DIFFLOGIC_AVAILABLE = True
+except ImportError as e:
+    print("Difflogic package is not installed. Please install it via 'pip install difflogic'.")
+    DIFFLOGIC_AVAILABLE = False
 import torch
 import torch.nn as nn
 
 from .config import Config
 from .model_config import MLPConfig, CNNConfig, DiffLogicConfig
 
+if DIFFLOGIC_AVAILABLE:
+    class DiffLogic(nn.Module):
+        def __init__(self, config: DiffLogicConfig, input_size: int, num_classes: int, device: torch.device = torch.device('cpu')):
+            super().__init__()
+            logic_layers = []
 
-class DiffLogic(nn.Module):
-    def __init__(self, config: DiffLogicConfig, input_size: int, num_classes: int, device: torch.device = torch.device('cpu')):
-        super().__init__()
-        logic_layers = []
+            llkw = dict(grad_factor=config.grad_factor, connections=config.connections)
 
-        llkw = dict(grad_factor=config.grad_factor, connections=config.connections)
+            ####################################################################################################################
 
-        ####################################################################################################################
-
-        logic_layers.append(torch.nn.Flatten())
-        logic_layers.append(
-            LogicLayer(
-                in_dim=input_size, 
-                out_dim=config.num_neurons, 
-                connections='random', 
-                grad_factor=config.grad_factor, 
-                device=device.type
-            )
-        )
-        for _ in range(config.num_layers - 1):
+            logic_layers.append(torch.nn.Flatten())
             logic_layers.append(
                 LogicLayer(
-                    in_dim=config.num_neurons, 
+                    in_dim=input_size, 
                     out_dim=config.num_neurons, 
-                    connections=config.connections,
+                    connections='random', 
                     grad_factor=config.grad_factor, 
                     device=device.type
                 )
             )
+            for _ in range(config.num_layers - 1):
+                logic_layers.append(
+                    LogicLayer(
+                        in_dim=config.num_neurons, 
+                        out_dim=config.num_neurons, 
+                        connections=config.connections,
+                        grad_factor=config.grad_factor, 
+                        device=device.type
+                    )
+                )
 
-        self.network = torch.nn.Sequential(
-            *logic_layers,
-            GroupSum(num_classes, config.tau)
-        )
+            self.network = torch.nn.Sequential(
+                *logic_layers,
+                GroupSum(num_classes, config.tau)
+            )
 
-    def forward(self, x: torch.Tensor):
-        x = self.network(x)
-        return x
+        def forward(self, x: torch.Tensor):
+            x = self.network(x)
+            return x
+else:
+    class DiffLogic(nn.Module):
+        def __init__(self, *args, **kwargs):
+            raise ImportError("Difflogic package is not installed. Please install it via 'pip install difflogic'.")
 
+        def forward(self, x: torch.Tensor):
+            pass
 
 class MLP(nn.Module):
     """Simple Multi-Layer Perceptron"""

src/train.py
@@ -36,29 +36,30 @@ def train_step(
     if data.dtype == torch.bool:
         data = data.float()
 
-    data, targets = data.to(device, dtype=dtype), targets.to(device)
+    data, targets = data.to(device), targets.to(device)
+    device_type = device.type if device.type != 'mps' else 'cpu'
 
+    with torch.autocast(device_type=device_type, dtype=dtype):
+        outputs = model(data)
+        loss = criterion(outputs, targets)
     optimizer.zero_grad()
-    outputs = model(data)
-    loss = criterion(outputs, targets)
     loss.backward()
     optimizer.step()
 
     # get sample accuracy
-    _, accuracy = get_correct_and_accuracy(outputs, targets)
+    correct, accuracy = get_correct_and_accuracy(outputs, targets)
 
     if config.base.debug:# and batch_idx % config.train.log_interval == 0:
         # print data, target and output stats for debugging
-        logger.debug(f"Batch {batch_idx}:")
-        print(targets.cpu().numpy().tolist())
-        # print the output tensor as indices of the max logits
-        print(outputs.argmax(dim=1).cpu().numpy().tolist())
+        # logger.debug(f"Batch {batch_idx}:")
+        # print(targets.cpu().numpy().tolist())
+        # # print the output tensor as indices of the max logits
+        # print(outputs.argmax(dim=1).cpu().numpy().tolist())
         
         logger.debug(f"  Loss: {loss.item():.4f}, Accuracy: {accuracy*100:.2f}%")
         # Uncomment the following line to log output tensor details
-    #     logger.debug(f"Device, dtype and shape of outputs: {outputs.device}, {outputs.dtype}, {outputs.shape}")
 
-    return loss.item(), accuracy
+    return loss.item(), correct, accuracy
 
 
 def train_epoch(
@@ -70,7 +71,7 @@ def train_epoch(
         config: config.Config, 
         start_time: float, 
         last_checkpoint_time: float
-    ) -> tuple[float, int, float]:
+    ) -> tuple[float, float, int, float]:
     """Train for one epoch"""
     model.train()
     total_loss = 0.0
@@ -79,55 +80,62 @@ def train_epoch(
     device = config.train.device if isinstance(config.train.device, torch.device) else torch.device(config.train.device)
     dtype = config.train.dtype if isinstance(config.train.dtype, torch.dtype) else getattr(torch, config.train.dtype)
 
-    with torch.autocast(device.type if device.type != 'mps' else 'cpu', dtype=dtype):
-        for batch_idx, batch in enumerate(dataloader):
-            loss, accuracy = train_step(
-                model=model, batch=batch, criterion=criterion, optimizer=optimizer, config=config, batch_idx=batch_idx, device=device, dtype=dtype
-            )
-            total_loss += loss
-            batch_count += 1
+    correct, total_samples = 0, 0
 
-            # Time-based checkpointing
-            current_time = time.time()
-            if current_time - last_checkpoint_time >= checkpoint_interval_seconds:
-                elapsed_time = current_time - start_time
-                logger.info(f"Batch {batch_count}, Loss: {loss:.4f}, Elapsed: {elapsed_time/60:.1f} minutes - Saving checkpoint...")
-                io_funcs.save_checkpoint(model=model, optimizer=optimizer, batch_count=batch_count, config=config, job_id=config.base.job_id, elapsed_time=elapsed_time)
-                last_checkpoint_time = current_time
+    for batch_idx, batch in enumerate(dataloader):
+        loss, batch_correct, accuracy = train_step(
+            model=model, batch=batch, criterion=criterion, optimizer=optimizer, config=config, batch_idx=batch_idx, device=device, dtype=dtype
+        )
+        batch_size = batch[0].shape[0]
+        total_loss += loss
+        correct += batch_correct
+        total_samples += batch_size
 
-            if batch_idx % config.train.log_interval == 0:
-                logger.info(f"  Batch {batch_idx}/{len(dataloader)}, Loss: {loss:.4f}, Accuracy: {100 * accuracy:.2f}%")
+        # Time-based checkpointing
+        current_time = time.time()
+        if current_time - last_checkpoint_time >= checkpoint_interval_seconds:
+            elapsed_time = current_time - start_time
+            logger.info(f"Batch {batch_count}, Loss: {loss:.4f}, Elapsed: {elapsed_time/60:.1f} minutes - Saving checkpoint...")
+            io_funcs.save_checkpoint(model=model, optimizer=optimizer, batch_count=batch_count, config=config, job_id=config.base.job_id, elapsed_time=elapsed_time)
+            last_checkpoint_time = current_time
 
-            if config.base.debug and batch_idx >= config.train.debugging_steps:
-                break  # For debugging, limit to specified batches per epoch
+        if batch_idx % config.train.log_interval == 0:
+            logger.info(f"  Batch {batch_idx}/{len(dataloader)}, Loss: {loss:.4f}, Accuracy: {100 * accuracy:.2f}%")
 
-    return total_loss / len(dataloader), batch_count, last_checkpoint_time
+        if config.base.debug and batch_idx >= config.train.debugging_steps:
+            break  # For debugging, limit to specified batches per epoch
+    batch_count += total_samples
+    accuracy = 100. * correct / total_samples if total_samples > 0 else 0.
+    loss_per_sample = total_loss / total_samples if total_samples > 0 else 0.
+    return loss_per_sample, accuracy, batch_count, last_checkpoint_time
 
 
 def evaluate(model: nn.Module, dataloader: DataLoader, criterion: nn.Module, config: config.Config) -> tuple[float, float]:
     """Evaluate model"""
     model.eval()
     total_loss = 0.0
-    corrects, total = 0, 0
+    corrects, total_samples = 0, 0
     dtype = config.train.dtype if isinstance(config.train.dtype, torch.dtype) else getattr(torch, config.train.dtype)
     device = config.train.device if isinstance(config.train.device, torch.device) else torch.device(config.train.device)
 
-    with torch.no_grad(), torch.autocast(device.type if device.type != 'mps' else 'cpu'):
-        for data_batch, targets in dataloader:
-            # Convert boolean data to float if necessary
-            if data_batch.dtype == torch.bool:
-                data_batch = data_batch.float()
+    device_type = device.type if device.type != 'mps' else 'cpu'
 
-            data_batch, targets = data_batch.to(device, dtype=dtype), targets.to(device)
+    for data_batch, targets in dataloader:
+        # Convert boolean data to float if necessary
+        if data_batch.dtype == torch.bool:
+            data_batch = data_batch.float()
+
+        data_batch, targets = data_batch.to(device), targets.to(device)
+        with torch.no_grad(), torch.autocast(device_type=device_type, dtype=dtype):
             outputs = model(data_batch)
             loss = criterion(outputs, targets)
 
-            total_loss += loss.item()
-            correct, _ = get_correct_and_accuracy(outputs, targets)
-            corrects += correct
-            total += targets.size(0)
+        total_loss += loss.item()
+        correct, _ = get_correct_and_accuracy(outputs, targets)
+        corrects += correct
+        total_samples += targets.size(0)
 
-    accuracy = 100. * corrects / total
-    avg_loss = total_loss / len(dataloader)
+    accuracy = 100. * corrects / total_samples if total_samples > 0 else 0
+    avg_loss = total_loss / total_samples if total_samples > 0 else 0
 
     return avg_loss, accuracy